e6de95875c6fbd89d1b11ac0893957ed485d0111
azrelHaikal <azrelhaikal@gmail.com>
2024-12-26 06:59:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("action-button").addEventListener("click", function () {
+    const message = document.getElementById("message");
+    message.textContent = "Daddy chill 🎉";
+    message.style.color = "#0073e6";
+});
